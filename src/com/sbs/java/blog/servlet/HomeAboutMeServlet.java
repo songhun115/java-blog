870a70a577298bd99779fb6100b6c63645576985
kimhyeji <gpwl5589@naver.com>
2020-06-25 13:55:54 +0900

    DB연동

--- src/com/sbs/java/blog/servlet/HomeAboutMeServlet.java
+++ src/com/sbs/java/blog/servlet/HomeAboutMeServlet.java
@@ -7,14 +7,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/home/aboutMe")
+@WebServlet("/s/home/aboutMe")
 public class HomeAboutMeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=utf-8");
 		request.getRequestDispatcher("/jsp/home/aboutMe.jsp").forward(request, response);
 	}
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
